3f741416ded32f9c926fa3e778f7189640698463
Joanna Bell <jjbdigital@gmail.com>
2022-08-02 15:11:22 -0500

    commit tree b8c4ced3f9626de1c6437bfdc7181f9ea941a682

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "doll", "ball"];
+
+function wrapGift(gift) {
+    console.log(`Wrapped ${gift} and added a bow!`);
+
+}
+
+wrapGift([0]);
+wrapGift([1]);
+wrapGift([2]);
